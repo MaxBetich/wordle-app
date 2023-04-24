2790d68234d29bc6d6eb06d2ab0628f5739389f4
Laura Hope <ssteamlaura@gmail.com>
2023-04-24 16:36:58 -0700

    Add functionality to onscreen keyboard.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,36 @@ import './css/styles.css';
 // Business Logic
 
 
+function fillCell(letter) {
+  const cells = document.querySelectorAll(".wordle-cell");
+  let emptyCell = null;
+  cells.forEach((cell) => {
+    if (!emptyCell && cell.value === "") {
+      emptyCell = cell;
+    }
+  });
+  if (emptyCell) {
+    emptyCell.value = letter;
+    emptyCell.classList.add("filled");
+  }
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key)=> {
+  key.addEventListener("click", () => {
+    const letter = key.innerText;
+    fillCell(letter);
+  });
+});
+
+const deleteKey = document.querySelector(".delete");
+deleteKey.addEventListener("click", () => {
+  const fullCell = document.activeElement;
+  if (fullCell.tagName === 'INPUT' && fullCell.value !== "" ) {
+    fullCell.value = "";
+    fullCell.classList.remove("filled");
+  }
+});
 
 
 
@@ -25,17 +55,19 @@ import './css/styles.css';
 
 // UI Logic
 
-// let deleteKey = document.querySelector(".delete");
+
+
+
 // let enterKey = document.querySelector(".enter");
 // let allKey = document.querySelectorAll(".key");
-// let textContainer = document.querySelector(".textContainer");
+// let wordleCell = document.querySelector(".wordle-cell");
 
 // for(let key of allKey){
 //   key.addEventListener("click",function(){
 //       if(key.classList.contains("delete") || key.classList.contains("enter")){
 //           return;
 //       }
-//       textContainer.innerText += key.innerText;
+//       wordleCell.innerText += key.innerText;
 //   })
 // }
 
