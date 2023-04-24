58e9b80ccc3f47db69da93726c30c3e6f55fbbe1
Laura Hope <ssteamlaura@gmail.com>
2023-04-24 15:19:28 -0700

    Add CSS styling, initial HTML template, and divs for word grid and interactive onscreen keyboard.

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+// import '';
+
+// Business Logic
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// UI Logic
+
+// let deleteKey = document.querySelector(".delete");
+// let enterKey = document.querySelector(".enter");
+// let allKey = document.querySelectorAll(".key");
+// let textContainer = document.querySelector(".textContainer");
+
+// for(let key of allKey){
+//   key.addEventListener("click",function(){
+//       if(key.classList.contains("delete") || key.classList.contains("enter")){
+//           return;
+//       }
+//       textContainer.innerText += key.innerText;
+//   })
+// }
+
+
+// window.addEventListener("load", function() {
+//   this.document.querySelector("").addEventListener("submit", handleFormSubmission);
+// });
+
