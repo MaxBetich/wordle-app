b16db4fa4eaf708a1025859b4f15532c35b39cde
Laura Hope <ssteamlaura@gmail.com>
2023-04-25 13:09:32 -0700

    Add modal, accordian with game instructions, more CSS styling features, and working backspace/delete button.

--- src/index.js
+++ src/index.js
@@ -30,46 +30,62 @@ keys.forEach((key)=> {
 
 const deleteKey = document.querySelector(".delete");
 deleteKey.addEventListener("click", () => {
-  const fullCell = document.activeElement;
-  if (fullCell.tagName === 'INPUT' && fullCell.value !== "" ) {
+  const cells = document.querySelectorAll(".wordle-cell");
+  let fullCell = null;
+  cells.forEach((cell) => {
+    if (cell.classList.contains("filled")) {
+      fullCell = cell;
+    }
+  });
+  if (fullCell) {
     fullCell.value = "";
     fullCell.classList.remove("filled");
+    const previousCell = fullCell.previousElementSibling;
+    if (previousCell) {
+      previousCell.focus();
+    }
   }
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // UI Logic
 
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
 
+openModalButtons.forEach(button =>{
+  button.addEventListener('click', () =>{
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
 
+overlay.addEventListener('click', () =>{
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach(modal =>{
+    closeModal(modal);
+  });
+});
 
-// let enterKey = document.querySelector(".enter");
-// let allKey = document.querySelectorAll(".key");
-// let wordleCell = document.querySelector(".wordle-cell");
+closeModalButtons.forEach(button =>{
+  button.addEventListener('click', () =>{
+    const modal = button.closest('.modal');
+    closeModal(modal);
+  });
+});
 
-// for(let key of allKey){
-//   key.addEventListener("click",function(){
-//       if(key.classList.contains("delete") || key.classList.contains("enter")){
-//           return;
-//       }
-//       wordleCell.innerText += key.innerText;
-//   })
-// }
+function openModal(modal){
+  if(modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+
+function closeModal(modal){
+  if(modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
 
 
 // window.addEventListener("load", function() {
